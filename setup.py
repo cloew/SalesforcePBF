010f9539fcefe6e31826a3f6082c7ebd65716d11
Chris Loew <cloew123@gmail.com>
2014-04-15 19:40:14 -0600

    Fixed setup so taht it installs the helper files as well

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(name='pbf.salesforce',
       version='.1',
       description="Programmer's Best Friend Utility Extension for Salesforce",
-      author='', # Add your name here
-      author_email='', # Add your e-mail here
-      packages=['pbf.salesforce', 'pbf.salesforce.Commands', 'pbf.salesforce.templates'],
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      packages=['pbf.salesforce', 'pbf.salesforce.Commands', 'pbf.salesforce.helpers', 'pbf.salesforce.templates'],
       #package_data = {'pbf.salesforce.templates':[]}, # Add template files
      )
